b18b1c60f1f6c16830c972bf56901016937cd617
Nett <goeginett@hotmail.com>
2016-12-29 16:49:43 +0100

    Klasse SequenzManager erstellt

---
+++ src/controller/SequenceManager.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.util.Observable;
+
+/**
+ * Die Klasse beschreibt den sequenziellen Ablauf der Applikation.
+ * Als Observable benachrichtigt sie die GUI über den Zustand der
+ * aktuell ausgeführten externen Applikation
+ *
+ * Created by Nett on 29.12.2016.
+ * @author Nett
+ */
+public class SequenceManager extends Observable {
+
+
+    public void start(){
+
+    }
+
+}
