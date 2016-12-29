3afcdfa93a81acb01763e84bd400261da2bd8b39
Nett <goeginett@hotmail.com>
2016-12-29 16:41:13 +0100

    Klasse StringVerifier erstellt um die Maschinennummer auf Gültigkeit zu prüfen.

---
+++ src/controller/StringVerifier.java
@@ -0,0 +1,103 @@
+package controller;
+
+import model.MachineNumberReader;
+import model.ReadWriteException;
+
+/**
+ * Verifiziert einen String ob er den Anforderungen gemäss Beschreibung entspricht
+ *
+ * Created by Nett on 29.12.2016.
+ * @author Nett
+ */
+public class StringVerifier {
+
+    private MachineNumberReader reader = null;
+
+    public StringVerifier() {
+        reader = new MachineNumberReader("C:\\tcommc\\exe\\machinnr.in");
+    }
+
+    public StringVerifier(MachineNumberReader reader) {
+        this.reader = reader;
+    }
+
+    /**
+     * Gibt die Maschinennummer zurück wenn sie gültig ist,
+     * sonst wir eine ReadWriteException geworfen.
+     *
+     * @return machNumber
+     * @throws ReadWriteException
+     */
+    public String getMachineNumber()throws ReadWriteException {
+        String machNumber = reader.readMachineNumber();
+        if(!isMachineNumberValid(machNumber)){
+            ReadWriteException rwEx = new ReadWriteException(machNumber + " ist keine gültige Maschinennnummer");
+            throw rwEx;
+        }
+        return machNumber;
+    }
+
+    /**
+     * Verifiziert einen String ober er folgenden Anforderungen entspricht.
+     *
+     * Die Maschinennummer darf weder Null noch Empty sein. Die Länge ist
+     * genau 10, und an der Stelle 0 und 5 muss ein Buchstabe sein.
+     * Alle anderen Ziffern müssen Zahlen sein.
+     *
+     * @param machNumber
+     * @return true wenn valid
+     */
+    private boolean isMachineNumberValid(String machNumber){
+        if(isStringNullOrEmpty(machNumber)){
+            return false;
+        }
+        if(machNumber.length() != 10){
+            return false;
+        }
+        if(!Character.isLetter(machNumber.charAt(0))
+                || !Character.isLetter(machNumber.charAt(5))){
+            return false;
+        }
+        if(!hasSubstringNumbersOnly(machNumber,1,4)){
+            return false;
+        }
+        if(!hasSubstringNumbersOnly(machNumber,6,machNumber.length())){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Prüft ob der String Null oder Empty ist.
+     *
+     * @param machNumber
+     * @return true wenn null oder Empty
+     */
+    private boolean isStringNullOrEmpty(String machNumber){
+        if(machNumber == null){
+            return true;
+        }
+        if(machNumber.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Prüft einen String ob er vom Index start bis end nur aus Zahlen besteht.
+     *
+     * @param machNumber zu prüfender String
+     * @param start
+     * @param end
+     * @return true wenn der String vom Index start bis end nur aus Zahlen besteht
+     */
+    private boolean hasSubstringNumbersOnly(String machNumber, int start, int end){
+        String shortName = machNumber.substring(start, end);
+        for (char c : shortName.toCharArray()){
+            if(!Character.isDigit(c)){
+                return  false;
+            }
+        }
+        return true;
+    }
+}
